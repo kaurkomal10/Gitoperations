7e5607c5b63489bcbd1bfe8806e67861d44f7cd4
kaurkomal10 <133712444+kaurkomal10@users.noreply.github.com>
2023-05-29 17:37:57 -0400

    Update Gitoperations.java

--- src/gitoperations/Gitoperations.java
+++ src/gitoperations/Gitoperations.java
@@ -16,7 +16,7 @@ public class Gitoperations {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("komal");
-        // branch is modified
+        // branch is modified ver4
     }
     
 }
